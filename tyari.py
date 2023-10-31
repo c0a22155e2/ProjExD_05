0e33d563ca61489ee9c874123f977612a32d6680
YAMADA <c0a22143c0@edu.teu.ac.jp>
2023-10-31 16:45:03 +0900

    作成途中

--- tyari.py
+++ tyari.py
@@ -1,13 +1,30 @@
 import sys
 import pygame as pg
 
+
+class Coin:
+    def __init__(self, bird: Tori):
+        """
+        コイン画像を生成する
+        """
+        self.imgs = list()
+        self.imgs.append(pygame.image.load(f"ex05/coin01_gold01"))
+        self.rct = self.img.get_rect()
+        
+    def update(self, screen: pg.Surface):
+        """
+        コインを速度に従って移動させる
+        """
+        self.rct.move_ip(self.vx, self.vy)
+        screen.blit(self.img, self.rct)  
+
 def main():
-    pg.display.set_caption("はばたけ！こうかとん")
+    pg.display.set_caption("チャリ走DX!!!!")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex05/figs/bg.png")
     bg_img2 = pg.transform.flip(bg_img, True ,False)
-    tori = pg.image.load("ex01/fig/3.png")
+    tori = pg.image.load("ex01-20230926/fig/3.png")
     tori1 = pg.transform.flip(tori, True ,False)
     tori2 = pg.transform.rotozoom(tori1, 10,1.0)
     t_list = [tori1 , tori2]
