46e61ab484b8ea0a1236293028d045b8014caab7
YAMADA <c0a22143c0@edu.teu.ac.jp>
2023-11-07 13:41:39 +0900

    コイン

--- tyari.py
+++ tyari.py
@@ -7,7 +7,16 @@ HEIGHT = 744
 
 #完成版
 
-class tyari():
+def check_bound(obj: pg.Rect) -> tuple[bool, bool]:
+    yoko, tate = True, True
+    if obj.left < 0 or WIDTH < obj.right:  # 横方向のはみ出し判定
+        yoko = False
+    if obj.top < 0 or HEIGHT < obj.bottom:  # 縦方向のはみ出し判定
+        tate = False
+    return yoko, tate
+
+
+class tyari(pg.sprite.Sprite):
 
     def __init__(self,num: int ,xy:tuple[int,int]):
 
@@ -21,7 +30,6 @@ class tyari():
         self.img = self.imgs[0]
         self.rct = self.img.get_rect()
         self.rct.center = xy
-
 
     def change_img(self, num: int, screen: pg.Surface):
 
@@ -43,13 +51,8 @@ class Coin(pg.sprite.Sprite):
         super(Coin, self).__init__()
         #画像をリストに代入する
         self.imgs = list()
-        self.imgs.append(pg.transform.rotozoom(pg.image.load(f"ex05/coin01_gold01/coin01_gold01_1.png"),0,0.5))
-        self.imgs.append(pg.transform.rotozoom(pg.image.load(f"ex05/coin01_gold01/coin01_gold01_2.png"),0,0.5))
-        self.imgs.append(pg.transform.rotozoom(pg.image.load(f"ex05/coin01_gold01/coin01_gold01_3.png"),0,0.5))
-        self.imgs.append(pg.transform.rotozoom(pg.image.load(f"ex05/coin01_gold01/coin01_gold01_4.png"),0,0.5))
-        self.imgs.append(pg.transform.rotozoom(pg.image.load(f"ex05/coin01_gold01/coin01_gold01_5.png"),0,0.5))
-        self.imgs.append(pg.transform.rotozoom(pg.image.load(f"ex05/coin01_gold01/coin01_gold01_6.png"),0,0.5))
-        
+        for i in range(1,7):
+            self.imgs.append(pg.transform.rotozoom(pg.image.load(f"ex05/coin01_gold01/{i}.png"),0,0.4))
         
         self.index = 0
         self.image = self.imgs[self.index]
@@ -61,6 +64,26 @@ class Coin(pg.sprite.Sprite):
         
         self.image = self.imgs[self.index]
         self.index += 1
+
+class Score:
+    """
+    コインとチャリンコが接したときにスコアを表示するクラス
+    1コイン　= 1ポイント
+    """
+    def __init__(self):
+        self.font = pg.font.Font(None, 50)
+        self.color = (0, 0, 255)
+        self.score = 0
+        self.image = self.font.render(f"Score: {self.score}", 0, self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = 100, HEIGHT-50
+    
+    def score_up(self, add):
+        self.score += add
+    
+    def update(self, screen: pg.Surface):
+        self.image = self.font.render(f"Score: {self.score}", 0, self.color)
+        screen.blit(self.image, self.rect)
         
 
 def main():
@@ -74,18 +97,26 @@ def main():
     tmr = 0
     x = tmr
     coin = Coin()
+    coins = pg.sprite.Group()
+    score = Score()
+    tyaris = pg.sprite.Sprite()
     coin_group = pg.sprite.Group(coin)
     
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+            
+            for coin in pg.sprite.groupcollide(coins, tyaris, True, True).keys():
+                score.score_up(1)
         
         screen.blit(bg_img, [0-x, 0])
         screen.blit(bg_img2, [1297-x,0])
         screen.blit(bg_img, [2594-x, 0])
         bird.update(screen)
+        score.update(screen)
         pg.draw.rect(screen,(255,255,255),(0,HEIGHT*0.8,WIDTH,HEIGHT))
-        coin_group.update()
+        if tmr % 3 == 1:
+            coin_group.update()
         coin_group.draw(screen)
         pg.display.update()
         tmr += 1        
